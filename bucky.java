d9e7fda937d78e320eb0fb3e29246ef7ceb9afa2
torberglund <tor.berglund@gmail.com>
2014-11-06 11:22:35 +0100

    hej

--- bucky.java
+++ bucky.java
@@ -1,14 +1,20 @@
+import java.util.*;
 public class bucky {
 	public static void main (String [] args) {
-	System.out.println(fact(4)+"");
-	};
+	
+	String[] things ={"eggs", "lasers", "hats","pie"};
+	List<String> list1 = new ArrayList<String>();
 
-	//fact
-	public static long fact(long n) {
-		if (n<=1)
-			return 1;
-		else
-			return n*fact(n-1);
+	//add arrayitems to list
+	for(String x : things)
+		list1.add(x);
 
+	String[] morethings ={"lasers","hats"};
+	List<String> list2 = new ArrayList<String>();
+
+	for(String x : morethings)
+		list2.add(x);
+	for(int i=0; i<list1.size(); i++)
+		System.out.printf("%s ", list1.get(i));
 	};
 };
