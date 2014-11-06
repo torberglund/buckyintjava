84b74ecd9ef5393b5629cbfe67463e741f15cde9
torberglund <tor.berglund@gmail.com>
2014-11-06 13:13:52 +0100

    hej

--- bucky.java
+++ bucky.java
@@ -2,39 +2,18 @@ import java.util.*;
 public class bucky {
 	public static void main (String [] args) {
 	
-	String[] things ={"eggs", "lasers", "hats","pie"};
-	List<String> list1 = new ArrayList<String>();
+	String[] things ={"apples","nuubs", "eggs", "lasers", "hats","pie"};
+	List<String> list1 = new LinkedList<String>();
 
-	//add arrayitems to list
 	for(String x : things)
 		list1.add(x);
 
-	String[] morethings ={"lasers","hats"};
-	List<String> list2 = new ArrayList<String>();
+	String [] things2 = {"sausage", "bacon", "goats", "harrypotter"};
+	List<String> list2 = new LinkedList<String>();
+	
+	for(String x : things2)
+	list2.add(x);
 
-	for(String x : morethings)
-		list2.add(x);
-
-	//print list1
-	for(int i=0; i<list1.size(); i++){
-		System.out.printf("%s ", list1.get(i));
-	};
-
-	editList(list1,list2);
-	System.out.println();
-
-	//print list1
-	for(int i=0; i<list1.size(); i++)
-		System.out.printf("%s ", list1.get(i));
-
-	};
-
-	public static void editList(Collection<String> l1,Collection<String>l2) {
-		Iterator<String> it = l1.iterator();
-		while(it.hasNext()){
-			if(l2.contains(it.next()))
-				it.remove();
-		};
 	};
 
 };
