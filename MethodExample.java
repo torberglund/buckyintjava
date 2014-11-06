69be65c2e196932208ffec9924bc858af38746a0
torberglund <tor.berglund@gmail.com>
2014-11-06 14:26:34 +0100

    generisk metod skapad

--- MethodExample.java
+++ MethodExample.java
@@ -1,11 +1,14 @@
 import java.util.*;
 public class MethodExample {
 	public static void main(String [] args) {
-		Integer [] iray = {1,2,3,4};
-		Character [] cray ={'a','b','c','d'};
+		//Integer [] iray = {1,2,3,4};
+		//Character [] cray ={'a','b','c','d'};
 
-		printMe(iray);
-		printMe(cray);
+		//printMe(iray);
+		//printMe(cray);
+
+		System.out.println(max(432,54,5443543));
+		System.out.println(max("apples","poop","beta"));
 
 
 	}
@@ -28,5 +31,15 @@ public class MethodExample {
 		for(T i :x )
 			System.out.printf("%s ",i);
 			System.out.println();
+	}
+
+	public static <T extends Comparable<T>> T max(T a, T b, T c){
+		T m = a;
+		if(b.compareTo(m) >0)
+			m= b;
+
+		if(c.compareTo(m) >0)
+			m= c;
+		return m;
 	}
 }
