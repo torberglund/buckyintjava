9846356931f56e8a116ed8ae4b6b3c26157ce280
torberglund <tor.berglund@gmail.com>
2014-11-06 13:30:32 +0100

    Added linkedlist functions

--- bucky.java
+++ bucky.java
@@ -13,7 +13,33 @@ public class bucky {
 	
 	for(String x : things2)
 	list2.add(x);
+	
+	list1.addAll(list2);
+	list2 = null;
+
+	printMe(list1);
+	removeStuff(list1, 2, 5);
+	printMe(list1);
+	reverseMe(list1);
+
 
 	};
+
+	private static void printMe(List<String> l) {
+		for(String s : l)
+			System.out.printf("%s ", s);
+		System.out.println();
+	}
+
+	private static void removeStuff(List<String> l, int from, int to) {
+		l.subList(from,to).clear();
+	}
+
+	private static void reverseMe(List<String> l) {
+		ListIterator<String> bobby = l.listIterator(l.size());
+		while(bobby.hasPrevious())
+			System.out.printf("%s ", bobby.previous());
+	}
+
 
 };
